09c22a0c5428956f61c68dcdae58ec3a8d008c2c
MytMnSthU <myatminsithu0072@gmail.com>
2023-07-30 10:56:03 +0630

    Add comment feature

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ import {
     FaSearch,
     FaTimes,
     FaTwitter,
+    FaUserAlt,
 } from "react-icons/fa";
-import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { createContext, isValidElement, useContext, useEffect, useRef, useState } from "react";
 import request, { gql } from "graphql-request";
 import PuffLoader from "react-spinners/PuffLoader";
 
@@ -122,12 +123,13 @@ const Nav = () => {
     );
 };
 
-const PrimaryButton = ({
+const LoadMoreButton = ({
     children,
     onClickBtn,
     isDisableBtn,
     btnLoading,
     setBtnLoading,
+    type = "button",
 }) => {
     return (
         <button
@@ -135,7 +137,7 @@ const PrimaryButton = ({
                 setBtnLoading(true);
                 onClickBtn();
             }}
-            type="button"
+            type={type}
             disabled={isDisableBtn}
             className=" px-8 py-4 text-base font-bold uppercase text-white bg-black border-2 border-black rounded-[15px] leading-none mt-5 disabled:cursor-not-allowed"
         >
@@ -144,11 +146,19 @@ const PrimaryButton = ({
     );
 };
 
-const SecondaryButton = ({ children }) => {
+const SubmitCommentButton = ({submitComment}) => {
+    return (
+        <button onClick={submitComment} className=" px-8 py-4 text-base font-bold uppercase text-white bg-black border-2 border-black rounded-[15px] leading-none mt-5 disabled:cursor-not-allowed">
+            Submit
+        </button>
+    );
+};
+
+const ResetButton = ({ type = "reset", children }) => {
     return (
         <button
-            type="button"
-            className=" px-12 py-4 text-base font-bold uppercase text-black bg-transparent border-2 border-black rounded-[15px]"
+            type={type}
+            className=" px-8 py-4 text-base font-bold uppercase text-black bg-transparent border-2 mt-5 leading-none border-black rounded-[15px]"
         >
             {children}
         </button>
@@ -229,9 +239,169 @@ const ArticleCard = ({ article }) => {
     );
 };
 
-const ArticleDetail = ({ articles }) => {
+const CommentBox = ({ comment }) => {
+    return (
+        <div className=" flex items-start gap-3">
+            <div className="w-fit relative">
+                <div className=" rounded-full bg-white p-2 border border-black relative z-10">
+                    <FaUserAlt />
+                </div>
+                <div className=" w-full h-full bg-black absolute rounded-full top-0 left-0 z-0 translate-x-[2px] translate-y-[2px]"></div>
+            </div>
+
+            <div className=" w-full relative">
+                <div className=" bg-white rounded-[15px] border border-black p-2.5 grid gap-1 relative z-10">
+                    <span className=" text-lg font-bold">
+                        {comment.username}
+                    </span>
+                    <TimeLabel time={comment.createdAt} />
+                    <p className=" text-xl px-3 font-medium mt-3">
+                        {comment.title}
+                    </p>
+                </div>
+                <div className=" w-full h-full bg-black rounded-[15px] absolute top-0 left-0 translate-x-1 translate-y-1"></div>
+            </div>
+        </div>
+    );
+};
+
+const CommentForm = ({ articleId , fetchAndSetArticles }) => {
+    const textRef = useRef(null);
+    const nameInputRef = useRef(null);
+    const emailInputRef = useRef(null);
+
+    const checkEmpty = (target) => {
+        return target.trim().length > 0;
+    };
+
+    const isEmailFormat = (target) =>{
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(target)
+    }
+
+    // console.log('valid?',isEmailFormat('aaaa'));
+
+    const postData = async (title,  username, email, articleId) => {
+        const endpoint = import.meta.env.VITE_REACT_APP_API_KEY;
+
+        const createCommentQuery = gql`
+            mutation CreateComment(
+                $id: ID,
+                $email: String,
+                $title: String,
+                $username: String
+            ) {
+                createComment(
+                    data: {
+                        clknsvk8e0s6j01uccy366fql: { connect: { id: $id } }
+                        email: $email
+                        title: $title
+                        username: $username
+                    }
+                ) {
+                    id
+                }
+            }
+        `;
+
+        const publishCommentQuery = gql`
+            mutation PublishComment($createdCommentId:ID) {
+                publishComment(
+                    where: { id: $createdCommentId }
+                    to: PUBLISHED
+                ) {
+                    createdAt
+                    id
+                    title
+                    username
+                }
+            }
+        `;
+
+        const createCommentVariables = {
+            email,
+            username,
+            title,
+            id: articleId,
+        };
+
+        const { createComment } = await request(
+            endpoint,
+            createCommentQuery,
+            createCommentVariables
+        );
+
+        const data = await request(endpoint, publishCommentQuery, {
+            createdCommentId: createComment.id,
+        });
+        console.log("Comments", data);
+
+        await fetchAndSetArticles();
+    };
+
+    const submitFormComment = () => {
+        const title = textRef?.current.value;
+        const username = nameInputRef?.current.value;
+        const email = emailInputRef?.current.value;
+
+        if (checkEmpty(title) && checkEmpty(username) && checkEmpty(email)) {
+
+            if(isEmailFormat(email)){
+                postData(title, username, email, articleId);
+            }else{
+                alert("Invalid email format");
+            }
+        }
+    };
+
+    return (
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                // submitFormComment();
+            }}
+            className=" grid gap-3"
+        >
+            <textarea
+                ref={textRef}
+                rows="4"
+                className=" w-full p-2 px-4 text-lg font-bold  rounded-[20px] border-2 border-black  placeholder:font-normal placeholder:text-black"
+                placeholder="Some text here..."
+            ></textarea>
+
+            <input
+                ref={nameInputRef}
+                type="text"
+                className="w-full p-2  px-4 text-lg font-bold rounded-[15px] border-2 border-black  placeholder:font-normal placeholder:text-black"
+                placeholder="Name"
+            />
+
+            <input
+                ref={emailInputRef}
+                type="email"
+                className="w-full p-2  px-4 text-lg font-bold rounded-[15px] border-2 border-black  placeholder:font-normal placeholder:text-black"
+                placeholder="Email"
+            />
+
+            <div className=" flex justify-end gap-5 items-center">
+                <ResetButton>Cancel</ResetButton>
+                <SubmitCommentButton
+                    submitComment={(e) => {
+                        e.preventDefault();
+                        submitFormComment();
+                    }}
+                >
+                    submit
+                </SubmitCommentButton>
+            </div>
+        </form>
+    );
+};
+
+const ArticleDetail = ({ articles, fetchAndSetArticles }) => {
     const contentRef = useRef(null);
     const { articleId } = useParams();
+    // const [Comments, setComments] = useState([]);
 
     const currentArticle = articles.filter(
         (article) => article.slug === articleId
@@ -265,7 +435,24 @@ const ArticleDetail = ({ articles }) => {
 
             <div ref={contentRef}></div>
 
+            <h4 className=" text-2xl font-extrabold">Comments</h4>
 
+            <div className=" grid gap-5 mb-10   ">
+                {currentArticle.comments.length === 0 ? (
+                    <div>
+                        <span className=" text-lg">No comment.</span>
+                    </div>
+                ) : (
+                    currentArticle.comments.map((comment) => (
+                        <CommentBox key={comment.id} comment={comment} />
+                    ))
+                )}
+            </div>
+
+            <div>
+                <h4 className=" text-2xl font-extrabold mb-4">Leave Comment</h4>
+                <CommentForm articleId={currentArticle.id} fetchAndSetArticles={fetchAndSetArticles} />
+            </div>
         </div>
     );
 };
@@ -449,14 +636,14 @@ const ArticleList = ({
                     </div>
                     {title.toLowerCase() === "recent articles" && (
                         <div className="flex justify-center items-center">
-                            <PrimaryButton
+                            <LoadMoreButton
                                 onClickBtn={moreBlog}
                                 isDisableBtn={isDisable}
                                 btnLoading={btnLoading}
                                 setBtnLoading={setBtnLoading}
                             >
                                 more blog
-                            </PrimaryButton>
+                            </LoadMoreButton>
                         </div>
                     )}
                 </div>
@@ -595,7 +782,7 @@ const Footer = ({ categories }) => {
 const App = () => {
     const [articles, setArticles] = useState([]);
     const [categories, setCategories] = useState([]);
-    const [currentArticlesCount, setCurrentArticlesCount] = useState(4);
+    const [currentArticlesCount, setCurrentArticlesCount] = useState(8);
     const [maxArticlesCount, setMaxArticlesCount] = useState(0);
     const [isDisable, setIsDisable] = useState(false);
     const [loading, setLoading] = useState(true);
@@ -630,6 +817,13 @@ const App = () => {
                     html
                 }
                 popularArticle
+                comments {
+                    email
+                    title
+                    username
+                    id
+                    createdAt
+                  }
             }
         }
     `;
@@ -667,15 +861,22 @@ const App = () => {
             const newArticle = {
                 ...article,
                 createdAt: formatDate(article.createdAt),
+                comments: article.comments.map((comment) => {
+                    const newComment = {
+                        ...comment,
+                        createdAt: formatDate(comment.createdAt),
+                    };
+                    return newComment;
+                }),
             };
 
             return newArticle;
         });
 
-
-
+        // console.log('format',formattedArticles);
         // sortByDesc(formattedArticles);
         // console.log(formattedArticles);
+        console.log(formattedArticles);
 
         setArticles(formattedArticles);
         setLoading(false);
@@ -795,6 +996,7 @@ const App = () => {
                                         element={
                                             <ArticleDetail
                                                 articles={articles}
+                                                fetchAndSetArticles={fetchAndSetArticles}
                                             />
                                         }
                                     />
