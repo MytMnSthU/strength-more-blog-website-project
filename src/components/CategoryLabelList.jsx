f75ade131da8145906b6579ee3752b5fa520a1e1
MytMnSthU <myatminsithu0072@gmail.com>
2025-10-03 08:25:08 +0630

    fix categories box error message style

--- src/components/CategoryLabelList.jsx
+++ src/components/CategoryLabelList.jsx
@@ -13,7 +13,12 @@ const CategoryLabelList = () => {
 
 	if (error) {
 		console.error("Error fetching categories:", error);
-		return <div>Error loading categories</div>;
+		return (
+			<div className="text-red-700 px-4 py-3 relative my-4" role="alert">
+				<strong className="font-bold">Error:</strong>
+				<span className="block sm:inline ml-2 text-lg">Failed to load categories.</span>
+			</div>
+		);
 	}
 
 	if (isLoading) return (
