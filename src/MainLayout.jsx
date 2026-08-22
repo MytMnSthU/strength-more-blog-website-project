bd2d42da2d034f2c0c5810acc7b9fd991aa4d8c6
MytMnSthU <myatminsithu0072@gmail.com>
2026-08-22 17:46:12 +0630

    adjusted sidebar width

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -13,8 +13,8 @@ const MainLayout = () => {
 			ref={scrollContainerRef}
 			className={`bg-[#F3F1E8] dark:bg-[#1F1F1F] text-black dark:text-white h-max border border-black`}>
 			<Nav toggleSearchModal={toggleSearchModal} />
-			<div className="container grid lg:grid-cols-3 xl:grid-cols-4 gap-5 p-2.5 mx-auto sm:pb-20 ">
-				<div className="sm:col-span-2 xl:col-span-3">
+			<div className="container grid lg:grid-cols-4 xl:grid-cols-4 gap-5 p-2.5 mx-auto sm:pb-20 ">
+				<div className="sm:col-span-3 xl:col-span-3">
 					<Outlet />
 				</div>
 				<div className=" ">
